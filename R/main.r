46c8faa994997f8b6c41dd3ea2e5b5cd44f6e265
crisoss <kujath@students.uni-marburg.de>
2021-01-16 15:44:59 +0100

    Initial commit

---
+++ R/main.r
@@ -0,0 +1,30 @@
+# R script from book "R Packages" written by Hadley Wickham, Jennifer Bryan
+# see: https://r-pkgs.org/index.html
+# 2021-01
+
+library(devtools)
+library(tidyverse)
+library(fs)
+#create_package("C:/PLAIT/C2R")
+
+(a <- factor(c("character", "hits", "your", "eyeballs")))
+# expected: "Levels: character eyeballs hits your"
+(b <- factor(c("but", "integer", "where it", "counts")))
+# expected: "Levels: but counts integer where it"
+c(a, b)
+factor(c(as.character(a), as.character(b)))
+# expected: "Levels: but character counts eyeballs hits integer where it your"
+
+# create new script defining (only!) fbind() <- function(a,b) {factor(c(as.character(a), as.character(b)))}
+
+# use_r("fbind")
+
+# load_all()
+# if load_all() throws an error:
+# rm(list = c("fbind"))
+# load_all()
+
+# fbind(a,b)
+# expected:"Levels: but character counts eyeballs hits integer where it your"
+
+exists("fbind", where = globalenv(), inherits = FALSE)
